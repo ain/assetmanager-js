c6dbaefa4490e33583be49f14a0aa70d65e5530d
Ain Tohvri <ain@flashbit.net>
2013-02-05 01:57:42 +0100

    Added Lint free source

--- src/AssetManager.js
+++ src/AssetManager.js
@@ -3,30 +3,33 @@ function AssetManager() {
   this.failCount = 0;
   this.cache = {};
   this.queue = [];
-};
+}
 AssetManager.prototype.add = function(id, path) {
   this.queue.push({id: id, path: path});
 };
 AssetManager.prototype.downloadAll = function(callback) {
   for (var i = 0, l = this.queue.length; i < l; i++) {
     var img = new Image();
-    var that = this;
-    img.addEventListener('load', function() {
-      that.successCount++;
-      that.cache[this.assetId] = this;
-      if (that.isDone()) {
-        callback.call();
-      }
-    }, false);
-    img.addEventListener('error', function() {
-      that.failCount++;
-    }, false);
     img.assetId = this.queue[i].id;
+    img.callbacks = {'load': callback};
+    img.manager = this;
+    img.addEventListener('load', this.handleImageLoadSuccess);
+    img.addEventListener('error', this.handleImageLoadError);
     img.src = this.queue[i].path;
   }
 };
+AssetManager.prototype.handleImageLoadSuccess = function() {
+  this.manager.successCount++;
+  this.manager.cache[this.assetId] = this;
+  if (this.manager.isDone()) {
+    this.callbacks.load.call();
+  }
+};
+AssetManager.prototype.handleImageLoadError = function() {
+  this.manager.failCount++;
+};
 AssetManager.prototype.isDone = function() {
-  return this.queue.length == this.successCount + this.failCount;
+  return this.queue.length === this.successCount + this.failCount;
 };
 AssetManager.prototype.getAsset = function(id) {
   return this.cache[id];
