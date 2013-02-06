6b4a077ce19ed016f058132a6f48482aa2cfae6a
Ain Tohvri <ain@flashbit.net>
2013-02-06 22:50:28 +0100

    Added support for Travis CI

--- grunt.js
+++ grunt.js
@@ -56,4 +56,6 @@ module.exports = function(grunt) {
   // Default task.
   grunt.registerTask('default', 'lint qunit concat min');
 
+  // Travis CI task
+  grunt.registerTask('travis', 'lint qunit');
 };
